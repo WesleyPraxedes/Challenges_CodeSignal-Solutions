a3b6cfe8b164d240ab2484a00c862ba2b61af35b
Wesley Praxedes <wesleyprofile@gmail.com>
2023-11-27 18:38:19 -0300

    Initial commit(Start project)

---
+++ Challenges_CodeSignal-Solutions/src/main/java/Challenges_CodeSignal_Solutions/Challenges_CodeSignal_Solutions/App.java
@@ -0,0 +1,13 @@
+package Challenges_CodeSignal_Solutions.Challenges_CodeSignal_Solutions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
